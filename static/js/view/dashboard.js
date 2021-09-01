3653a28f3475232081a875cd2aed74df5f43f018
N-Musa <n.musa@gmx.net>
2021-09-01 19:18:28 +0200

    update dashboard

--- static/js/view/dashboard.js
+++ static/js/view/dashboard.js
@@ -9,6 +9,8 @@ export default class extends abstractView {
         return ` 
         <h1>Welcome To Dashboard<h1>
         <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Dolore, doloribus!
+        Lorem, ipsum dolor sit amet consectetur adipisicing elit.
+         Libero pariatur a aut delectus impedit quod illo molestiae dolor optio unde.
         </p>
         <p>
             <a href="/posts" data-link>View recent articles</a>
