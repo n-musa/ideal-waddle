5b49522c6fd63fbed3f438f522892d32ee023fb7
N-Musa <n.musa@gmx.net>
2021-09-01 19:41:36 +0200

    Added handler limit

--- server.js
+++ server.js
@@ -6,9 +6,13 @@ const app = express();
 //Middleware
 app.use("/static", express.static(path.resolve("static")));
 
-app.get("/*", (req, res) => {
-    res.sendFile(path.resolve("index.html"));
-});
+
+
+app.get('/*', (req, res) => {
+    let path = req.params.path;
+    if (isValidPath(path))
+      res.sendFile(path);
+  });
 
 app.listen(process.env.PORT || 3000, () =>{
     console.log("Up & Running...");
